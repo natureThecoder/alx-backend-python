e822973d3bb397f6286acb5922aee4c520e80f03
natureThecoder <basilosinachi1@gmail.com>
2023-08-04 16:42:40 +0000

    fixing task one

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -3,7 +3,10 @@
                  as arguments and returns their sum as a float
     Parameters: a: float
                 b: float
+        Returns:
+        float: The sum of a and b.
 '''
+
 
 def add(a: float, b: float) -> float:
     return a + b
