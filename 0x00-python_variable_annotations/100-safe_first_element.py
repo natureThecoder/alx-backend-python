15ccea75547c78dcb276e31cefac1f6f61776592
natureThecoder <basilosinachi1@gmail.com>
2023-08-04 17:51:05 +0000

    fixing

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -8,6 +8,7 @@ from typing import Sequence, Any, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    ''' Outputs the first element of lst if there is any, otherwise None. '''
     if lst:
         return lst[0]
     else:
